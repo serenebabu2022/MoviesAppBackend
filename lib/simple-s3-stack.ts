5968381577cb446cb3b7cc3ddf92f3c3e15a4839
serenebabu2022 <serenebabu22@gmail.com>
2024-05-12 22:56:23 +0100

    Initial commit

--- lib/simple-s3-stack.ts
+++ lib/simple-s3-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import { CfnOutput, Duration, RemovalPolicy } from "aws-cdk-lib";
 
 export class SimpleS3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const s3Bucket = new s3.Bucket(this, 'SiteBucket', {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      // encryptionKey: new kms.Key(this, 's3BucketKMSKey'),
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      websiteIndexDocument: "index.html",
+    });
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./site")],
+      destinationBucket: s3Bucket,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleS3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, "WebsiteURL", {
+      value: s3Bucket.bucketWebsiteUrl,
+    });
   }
 }
